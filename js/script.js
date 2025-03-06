4ffebce12be18c2103fa888a284181f0a8e1cfa0
dacasfilipe <filiperdacas@gmail.com>
2025-03-06 20:15:19 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function salvar(){
     let idade = document.getElementById("idade").value;
     let email = document.getElementById("email").value;
     let senha = document.getElementById("senha").value;
+    //objeto javascript
     let usuario = {
         nome: nome,
         idade: idade,
@@ -18,26 +19,51 @@ function salvar(){
     let dados = JSON.parse(usuarioSalvo);
     console.log(dados);
     console.log(dados.idade);
-}
+    //enviar dados para o backend
+    fetch(
+        "http://localhost:8080/api/users",{
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+            },
+            body: JSON.stringify(usuario),
+        })
+        .then((response)=>response.json())
+        .then((data)=> {console.log("sucesso:",data);
 
-let nome = "Filipe";
-let idade = 35;
-const cidade = "Criciúma";
-let a = 2;
-let b = 2;
-let soma = a + b;
-//sout 
-console.log("Nome:"+nome);
-console.log("Idade:"+idade);
-console.log("Soma:"+ (a+b));
-console.log("Soma:"+soma);
-
-let pessoa ={
-    nome: "Filipe",
-    idade: 38,
-    email: "filipe@gmail.com",
-    senha: "123"
+        }).catch((error)=>{
+            console.error("erro:",error)});
 }
-console.log("Nome:"+pessoa.nome);
-console.log("Idade:"+pessoa.idade);
-console.log(pessoa);
+function salvarProduto(){
+    let name = document.getElementById("name").value;
+    let price = document.getElementById("price").value;
+    let description = document.getElementById("description").value;
+    let imgUrl = document.getElementById("imgUrl").value;
+    //objeto javascript
+    let product = {
+        name: name,
+        price: price,
+        description: description,
+        imgUrl: imgUrl
+    }
+    //converte o objeto para json
+    localStorage.setItem("product", JSON.stringify(product));
+    alert("produto salvo com sucesso." + product);
+    let productSalvo = localStorage.getItem("product");
+    // converte o json para objeto
+    let dados = JSON.parse(productSalvo);
+   //enviar dados para o backend
+    fetch(
+        "http://localhost:8080/api/products",{
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+            },
+            body: JSON.stringify(products),
+        })
+        .then((response)=>response.json())
+        .then((data)=> {console.log("sucesso:",data);
+
+        }).catch((error)=>{
+            console.error("erro:",error)});
+}
